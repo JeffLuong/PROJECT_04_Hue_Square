7701605476ce3df8a1984af175ecf23b0a34821e
Jeffrey Luong <jjck1988@gmail.com>
2015-08-06 17:59:49 -0400

    getting heroku setup

---
+++ server.js
@@ -0,0 +1,47 @@
+var express            = require('express'),
+    server             = express(),
+    mongoose           = require('mongoose'),
+    morgan             = require('morgan'),
+    session            = require('express-session'),
+    url                = 'mongodb://localhost:27017/hue_db';
+    // port               = 3000;
+
+// This sets PORT to the process PORT, if it doesn't exist, use local port.
+var PORT = process.env.PORT || 3000;
+var MONGOURI = process.env.MONGOLAB_URI || url;
+
+// MORGAN ERROR DETECTION
+server.use(morgan('short'));
+
+// SESSIONS
+server.use(session({
+  secret: "mysecret",
+  resave: true,
+  saveUninitialized: false
+}));
+
+// STATIC FILES
+server.use(express.static("./public"));
+
+// LANDING PAGE
+server.get('/', function(req, res) {
+  res.render('index');
+});
+
+server.get('/about', function(req, res) {
+  res.render('about');
+});
+
+mongoose.connect(MONGOURI);
+var db = mongoose.connection;
+
+db.on('error', function() {
+  console.log("Database error");
+});
+
+db.once('open', function() {
+  console.log("Database up and running");
+  server.listen(PORT, function() {
+    console.log("Server up and running");
+  });
+});
