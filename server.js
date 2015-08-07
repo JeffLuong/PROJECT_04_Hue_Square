a09c0cf66203ca1c4cd82b143e25147d99ed10ab
Jeffrey Luong <jjck1988@gmail.com>
2015-08-07 11:26:24 -0400

    removed mongo stuff for now...

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ var express            = require('express'),
     server             = express(),
     mongoose           = require('mongoose'),
     morgan             = require('morgan'),
-    session            = require('express-session'),
-    url                = 'mongodb://localhost:27017/hue_db';
+    session            = require('express-session');
+    // url                = 'mongodb://localhost:27017/hue_db';
 
 // This sets PORT to the process PORT, if it doesn't exist, use local port.
-var PORT = process.env.PORT || 3000;
+// var PORT = process.env.PORT || 3000;
 // var MONGOURI = process.env.MONGOLAB_URI || url;
 
 // MORGAN ERROR DETECTION
@@ -34,13 +34,13 @@ server.get('/about', function(req, res) {
 // mongoose.connect(MONGOURI);
 // var db = mongoose.connection;
 
-db.on('error', function() {
-  console.log("Database error");
-});
-
-db.once('open', function() {
-  console.log("Database up and running");
+// db.on('error', function() {
+//   console.log("Database error");
+// });
+//
+// db.once('open', function() {
+//   console.log("Database up and running");
   server.listen(3000, function() {
     console.log("Server up and running");
   });
-});
+// });
