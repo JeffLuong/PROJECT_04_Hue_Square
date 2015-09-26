177d93ccdead37e8741f35b9272dbf352b08c46f
Jeffrey Luong <jjck1988@gmail.com>
2015-09-26 14:30:21 -0400

    added loading screen

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
+
+  function onReady(callback) {
+    var interval = window.setInterval(checkReady, 250);
+    function checkReady() {
+      if ($("body")[0] !== undefined) {
+        window.clearInterval(interval);
+        callback.call(this);
+      }
+    }
+  }
+
+  onReady(function() {
+    $("#loading").fadeOut(250);
+    $("#main-container").fadeIn(350);
+  });
+
   window.game = new Game(GameControls, GameRenderer, GameData);
 });
